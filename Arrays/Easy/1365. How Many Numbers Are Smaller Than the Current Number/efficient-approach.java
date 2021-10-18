c4b8bd14e6af2c39dbb387a8b6093f2e326d4257
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2021-10-18 13:17:16 +0530

    Create efficient-approach.java
    **Ques Link**
    
    https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/

---
+++ Arrays/Easy/1365. How Many Numbers Are Smaller Than the Current Number/efficient-approach.java
@@ -0,0 +1,23 @@
+//O(N) Time
+//O(N) Space
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[101];
+        for(int i = 0; i<nums.length; i++){
+            ans[nums[i]]++;
+        }
+        for(int i = 1; i< 101; i++){
+            ans[i]+= ans[i-1];
+        }
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] == 0){
+                nums[i] = 0;
+            }
+            else{
+            nums[i] = ans[nums[i]-1];
+            }
+        }
+        return nums;
+    }
+}
