745a8e1d94376217078b776b97df150a083984bc
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2021-10-27 21:30:25 +0530

    https://leetcode.com/problems/valid-perfect-square/

---
+++ Arrays/Easy/367. Valid Perfect Square/efficient-approach.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+}
