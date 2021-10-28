ce2bd78e5dc12d4b2cc828aaef3b94e42182ef1f
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2021-10-28 12:29:46 +0530

    https://leetcode.com/problems/search-insert-position/

---
+++ Arrays/Easy/35. Search Insert Position/efficient-approach.java
@@ -0,0 +1,22 @@
+// Binary Search Solution
+// Time complexity: O(logn) and Space: O(1)
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length -1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid -1;
+            }
+        }
+        return end+1;
+    }
+}
