a93bdfa77e0fa469ad4d9f673fc936ea44044019
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2021-10-28 13:06:53 +0530

    Create efficient-approach.java

---
+++ Arrays/Easy/1351. Count Negative Numbers in a Sorted Matrix/efficient-approach.java
@@ -0,0 +1,18 @@
+//Time : O(M+N)
+
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int N = grid.length, row = N - 1, col = 0, M = grid[0].length, count = 0;
+        
+        while(row >= 0 && col < M){
+            if(grid[row][col] < 0){
+                count += M - col;
+                row--;
+            }else{
+                col++;
+            }
+        }
+        
+        return count;
+    }
+}
