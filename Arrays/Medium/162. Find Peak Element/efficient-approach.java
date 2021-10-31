fade75484ec7945bbba66e527e230f1203717581
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2021-10-31 12:29:00 +0530

    https://leetcode.com/problems/find-peak-element/

---
+++ Arrays/Medium/162. Find Peak Element/efficient-approach.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] a) {
+        int low = 0, mid = 0, high = a.length - 1;
+        while(low < high) {
+            mid = low + (high-low)/2;
+            if(a[mid] < a[mid+1]) low = mid+1;
+            else high = mid;
+        }
+        return low;
+    }
+}
