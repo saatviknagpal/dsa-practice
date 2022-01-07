dbecf840976a0a5b0c82bb0c8ebaa8023ecd78d6
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-01-07 19:05:42 +0530

    Time: 2 ms (95.52%), Space: 37.6 MB (55.93%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -2,22 +2,19 @@ class Solution {
     public boolean halvesAreAlike(String s) {
         String a = s.substring(0,s.length()/2);
         String b = s.substring(s.length()/2);
+        String vowels = "AEIOUaeiou";
         int count1 = 0;
         int count2 = 0;
+        
         for(int i = 0; i < a.length(); i++){
-            if(a.charAt(i) == 'a' || a.charAt(i) == 'e' || a.charAt(i) == 'i' || a.charAt(i) == 'o' || a.charAt(i) == 'u' || a.charAt(i) == 'A' || a.charAt(i) == 'E' || a.charAt(i) == 'I' || a.charAt(i) == 'O' || a.charAt(i) == 'U'){
+            if(vowels.indexOf(a.charAt(i)) >= 0){
                 count1++;
             }
-        }
-        for(int i = 0; i < b.length(); i++){
-            if(b.charAt(i) == 'a' || b.charAt(i) == 'e' || b.charAt(i) == 'i' || b.charAt(i) == 'o' || b.charAt(i) == 'u' || b.charAt(i) == 'A' || b.charAt(i) == 'E' || b.charAt(i) == 'I' || b.charAt(i) == 'O' || b.charAt(i) == 'U'){
+            if(vowels.indexOf(b.charAt(i)) >= 0){
                 count2++;
             }
         }
-        if(count1 == count2){
-            return true;
-        }
-        return false;
+        return count1 == count2;
         
     }
 }
