7bd3d5df29c8e5747c18d34b30c7ff8ee3839389
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-01-07 17:42:02 +0530

    Time: 2 ms (17.33%), Space: 39 MB (12.91%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,5 +1,10 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            ans.append(c > 64 && c < 91 ? (char)(c + 32) : c);
+        }
+        return new String(ans);
     }
 }
