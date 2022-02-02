38cf6d6d668438b60a82b7f327de8d869a18cdb0
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-02-02 13:10:04 +0530

    Time: 0 ms (100.00%), Space: 39 MB (6.95%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,15 @@
 class Solution {
+
     public int climbStairs(int n) {
         if (n == 1) return 1;
-        
-        // An array that represents the answer to the problem for a given state
-        int[] dp = new int[n + 1]; 
-        dp[1] = 1; // Base cases
-        dp[2] = 2; // Base cases
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2]; // Recurrence relation
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        
+
         return dp[n];
     }
 }
