d3fd7e260b983d3748d23cc06e35dbc502be9355
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-02-02 14:02:27 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (5.18%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,23 +1,17 @@
 class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
-    private int[] nums;
-
-    public int dp(int i) {
-        if (i == 0) {
-            return nums[0];
-        }
-        if (i == 1) {
-            return Math.max(nums[0], nums[1]);
-        }
-
-        if (!map.containsKey(i)) {
-            map.put(i, Math.max(dp(i - 1), dp(i - 2) + nums[i]));
-        }
-        return map.get(i);
-    }
 
     public int rob(int[] nums) {
-        this.nums = nums;
-        return dp(nums.length - 1);
+        if (nums.length == 1) return nums[0];
+
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[nums.length - 1];
     }
 }
