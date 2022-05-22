c7578b4098e088f5c644b2a52f7d136731b60668
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-05-22 19:51:17 +0530

    Time: 1 ms (99.99%), Space: 45.5 MB (98.81%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] num) {
+       int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
+    }
+}
