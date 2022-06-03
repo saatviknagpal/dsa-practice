72d403aff7cd145774811ee638f7990be0552b87
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-06-03 12:47:40 +0530

    Time: 18 ms (28.56%), Space: 47.3 MB (5.36%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+
+            if (!map.containsKey(c)) {
+                return false;
+            } else if (map.get(c) == 1) {
+                map.remove(c);
+            } else {
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        return map.size() == 0;
+    }
+}
