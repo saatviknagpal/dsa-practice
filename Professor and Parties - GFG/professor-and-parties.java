c2dc67627c83f83723ddb3db9271f805f208cd7b
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-06-03 12:14:46 +0530

    Added solution - LeetHub

---
+++ Professor and Parties - GFG/professor-and-parties.java
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.PartyType(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+
+    public static String PartyType( long a[], int n)
+    {
+        HashSet<Long> set = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            if(set.contains(a[i])){
+                return "BOYS";
+            }
+            set.add(a[i]);
+        }
+        return "GIRLS";
+    }
+}
