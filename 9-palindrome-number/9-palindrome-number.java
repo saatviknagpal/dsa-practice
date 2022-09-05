55ebae6de694d843c1e1fba133b192ef008942f3
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-09-05 13:10:52 +0530

    Time: 9 ms (98.07%), Space: 41.3 MB (98.47%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,5 +1,6 @@
 class Solution {
-    public boolean isPalindrome(int x) {
+    public boolean isPalindrome(int x) { 
+        if(x < 0) return false;
         String temp = Integer.toString(x);
         int i = 0;
         int j = temp.length() - 1;
