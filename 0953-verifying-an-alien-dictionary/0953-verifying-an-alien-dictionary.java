70989f726844cc91b594b4071cd9fea541fc409f
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2023-02-02 17:36:46 +0530

    Time: 1 ms (65.29%), Space: 41.9 MB (62.49%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -1,4 +1,9 @@
 class Solution {
+    
+    // first we put every single character from order in a hashmap to lexicographically sort it as per english language
+    // next we pick up two words from the array and check each character; if a character in first word greater than the second one, return false
+    // corner case - if the first word is of greater length from 2nd word but the first word starts with the 2nd word return false as the less length word should come before according to dictionary
+    
     public boolean isAlienSorted(String[] words, String order) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < order.length(); i++) {
@@ -8,7 +13,7 @@ class Solution {
             String word1 = words[i - 1];
             String word2 = words[i];
             int len = Math.min(word1.length(), word2.length());
-            if(len != word1.length() && len == word2.length() && word1.startsWith(word2)){
+            if (len != word1.length() && len == word2.length() && word1.startsWith(word2)) {
                 return false;
             }
             for (int j = 0; j < len; j++) {
@@ -19,7 +24,6 @@ class Solution {
                     break;
                 }
             }
-            
         }
         return true;
     }
