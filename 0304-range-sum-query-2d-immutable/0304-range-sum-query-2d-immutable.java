051d0fdb9a3af2711946ba726c97d8a89c93da5d
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2023-04-20 18:07:51 +0530

    Time: 132 ms (54.08%), Space: 68.8 MB (66.81%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,18 +1,23 @@
 class NumMatrix {
-    private int[][] data;
+    public int[][] prefixsum = new int[201][201];
 
     public NumMatrix(int[][] matrix) {
-        data = matrix;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        prefixsum = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefixsum[i][j] = prefixsum[i - 1][j] + prefixsum[i][j - 1] - prefixsum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for (int r = row1; r <= row2; r++) {
-            for (int c = col1; c <= col2; c++) {
-                sum += data[r][c];
-            }
-        }
-        return sum;
+        row1++;
+        row2++;
+        col1++;
+        col2++;
+        return prefixsum[row2][col2] - prefixsum[row2][col1 - 1] - prefixsum[row1 - 1][col2] + prefixsum[row1 - 1][col1 - 1];
     }
 }
 /**
