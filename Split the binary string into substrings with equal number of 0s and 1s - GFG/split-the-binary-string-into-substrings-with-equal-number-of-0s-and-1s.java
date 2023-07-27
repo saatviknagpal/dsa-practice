25155342a302f446cccdc5440083201752dd569d
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2023-07-27 12:55:35 +0530

    Added solution - LeetHub

---
+++ Split the binary string into substrings with equal number of 0s and 1s - GFG/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    public static int maxSubStr(String str) {
+       int vote = 0;
+       int ans = 0;
+       for(int i = 0; i < str.length(); i++){
+           char c = str.charAt(i);
+           if(c == '0'){
+               vote++;
+           } else if(c == '1'){
+               vote--;
+           }
+           
+           if(vote == 0){
+               ans++;
+           }
+       }
+       
+       if(vote != 0){
+           return -1;
+       } else {
+           return ans;
+       }
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	    var sc = new FastReader();
+	    int test = sc.nextInt();
+	    while(test-->0){
+    		String s = sc.nextString();
+    		System.out.println(Solution.maxSubStr(s));
+	    }
+	}
+    
+    public static class FastReader
+	{
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader()
+		{
+			br = new BufferedReader(new InputStreamReader(System.in));
+			st = new StringTokenizer("");
+		}
+
+		private void read() throws IOException
+		{
+			st = new StringTokenizer(br.readLine());
+		}
+
+		public String nextString() throws IOException
+		{
+			while(!st.hasMoreTokens())
+				read();
+			return st.nextToken();
+		}
+
+		public int nextInt() throws IOException
+		{
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() throws IOException
+		{
+			return Long.parseLong(nextString());
+		}
+	}
+}
+// } Driver Code Ends
