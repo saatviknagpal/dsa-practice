a0dbb23e85db5a21780e526805f76e2f82a6f842
Saatvik Nagpal <nagpal.saa@northeastern.edu>
2023-10-05 10:55:39 -0400

    Time: 71 ms (41.37%), Space: 17.3 MB (12.77%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -18,40 +18,40 @@ class Slist():
 
 class MyCircularQueue:
     def __init__(self, k: int):
-        #NOTHING CAN BE CHANGED HERE
-        self._K = k 
+        self._K = k
         self._s = Slist()
 
     def enQueue(self, value: int) -> bool:
-        newNode = ListNode(value)
-        if self._s._len != self._K:
-            self._s._len += 1
-            if self._s._first != None:
+        if self._s._len < self._K:
+            newNode = ListNode(value)
+            if self._s._first is None:
+                self._s._first = self._s._last = newNode
+            else:
                 self._s._last.next = newNode
                 self._s._last = newNode
-            else:
-                self._s._first = self._s._last = newNode
+            self._s._last.next = self._s._first
+            self._s._len += 1
             return True
         else:
             return False
 
-
     def deQueue(self) -> bool:
-        if self._s._first != None:
+        if self._s._first is not None:
             self._s._len -= 1
-            if self._s._first != self._s._last:
-                self._s._first = self._s._first.next
-            else:
+            if self._s._first == self._s._last:
                 self._s._first = self._s._last = None
+            else:
+                self._s._last.next = self._s._first.next
+                self._s._first = self._s._first.next
             return True
         else:
             return False
 
     def Front(self) -> int:
-        return self._s._first.val if self._s._first else -1
+        return self._s._first.val if not self.isEmpty() else -1
 
     def Rear(self) -> int:
-        return self._s._last.val if self._s._last else -1
+        return self._s._last.val if not self.isEmpty() else -1
 
     def isEmpty(self) -> bool:
         return self._s._len == 0
