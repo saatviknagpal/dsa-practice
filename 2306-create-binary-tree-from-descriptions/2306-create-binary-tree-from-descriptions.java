9b4e11b49a54151a11a7614ad95f5717678aca8a
Saatvik Nagpal <saatvikn@usc.edu>
2024-07-15 13:15:30 -0700

    Time: 74 ms (33.44%), Space: 58.7 MB (6.05%) - LeetHub

---
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Set<Integer> childrenSet = new HashSet<>();
+        Map<Integer, int[]> childrenHashmap = new HashMap<>();
+
+        for (int[] desc : descriptions) {
+            int parent = desc[0];
+            int child = desc[1];
+            boolean isLeft = desc[2] == 1;
+
+            childrenHashmap.putIfAbsent(parent, new int[]{-1, -1});
+            childrenSet.add(child);
+
+            if (isLeft) {
+                childrenHashmap.get(parent)[0] = child;
+            } else {
+                childrenHashmap.get(parent)[1] = child;
+            }
+        }
+
+        int headNodeVal = 0;
+        for (int parent : childrenHashmap.keySet()) {
+            if (!childrenSet.contains(parent)) {
+                headNodeVal = parent;
+                break;
+            }
+        }
+
+        return constructTree(headNodeVal, childrenHashmap);
+    }
+
+    private TreeNode constructTree(int curNodeVal, Map<Integer, int[]> childrenHashmap) {
+        TreeNode newNode = new TreeNode(curNodeVal);
+        if (childrenHashmap.containsKey(curNodeVal)) {
+            int[] children = childrenHashmap.get(curNodeVal);
+            if (children[0] != -1) {
+                newNode.left = constructTree(children[0], childrenHashmap);
+            }
+            if (children[1] != -1) {
+                newNode.right = constructTree(children[1], childrenHashmap);
+            }
+        }
+        return newNode;
+    }
+}
