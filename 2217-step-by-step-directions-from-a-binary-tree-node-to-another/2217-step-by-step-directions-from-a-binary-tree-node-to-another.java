30d02e49f7864f26fa20188ed5ae6a45c6c5a0e9
Saatvik Nagpal <saatvikn@usc.edu>
2024-07-15 21:18:55 -0700

    Time: 23 ms (67.29%), Space: 69.6 MB (96.58%) - LeetHub

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -1,68 +1,48 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public String getDirections(TreeNode root, int startValue, int destValue) {
-        List<Character> pathToStart = new ArrayList<>();
-        List<Character> pathToDest = new ArrayList<>();
-
-        findPath(root, startValue, new StringBuilder(), pathToStart);
-        findPath(root, destValue, new StringBuilder(), pathToDest);
-
-        int i = 0;
-        while (i < pathToStart.size() && i < pathToDest.size() && pathToStart.get(i) == pathToDest.get(i)) {
-            i++;
-        }
-
-        StringBuilder result = new StringBuilder();
-
-        for (int j = i; j < pathToStart.size(); j++) {
-            result.append('U');
-        }
-
-        for (int j = i; j < pathToDest.size(); j++) {
-            result.append(pathToDest.get(j));
-        }
-
-        return result.toString();
+        TreeNode ancestor = lowestCommonAncestor(root, startValue, destValue);
+        List<String> toStart = new ArrayList<>();
+        getDirection(ancestor, startValue, toStart);
+        List<String> toDest = new ArrayList<>();
+        getDirection(ancestor, destValue, toDest);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < toStart.size(); i++) sb.append("U");
+        for (String s : toDest) sb.append(s);
+        return sb.toString();
     }
-
-    private boolean findPath(TreeNode root, int value, StringBuilder path, List<Character> result) {
-        if (root == null) {
-            return false;
-        }
-        if (root.val == value) {
-            for (char c : path.toString().toCharArray()) {
-                result.add(c);
-            }
-            return true;
-        }
-
-        path.append('L');
-        if (findPath(root.left, value, path, result)) {
-            return true;
-        }
-        path.deleteCharAt(path.length() - 1);
-
-        path.append('R');
-        if (findPath(root.right, value, path, result)) {
-            return true;
-        }
-        path.deleteCharAt(path.length() - 1);
-
+    
+    private TreeNode lowestCommonAncestor(TreeNode root, int p, int q) {
+        if (root == null || p == root.val || q == root.val) return root; 
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root; 
+        else return left == null ? right : left;
+    }
+    
+    private boolean getDirection(TreeNode ancestor, int value, List<String> steps) {
+        if (ancestor == null) return false;
+        if (ancestor.val == value) return true;
+        steps.add("L");
+        if (getDirection(ancestor.left, value, steps)) return true;
+        steps.remove(steps.size() - 1);
+        steps.add("R");
+        if (getDirection(ancestor.right, value, steps)) return true;
+        steps.remove(steps.size() - 1);
         return false;
     }
-
 }
