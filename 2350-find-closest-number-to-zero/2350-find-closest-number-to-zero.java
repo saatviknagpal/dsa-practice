812aaa49ec555b69c503ecbf3d161730f27a4a3c
Saatvik Nagpal <saatvikn@usc.edu>
2024-08-01 12:20:32 -0700

    Time: 2 ms (90.53%), Space: 44.9 MB (44.96%) - LeetHub

---
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int ans = Integer.MIN_VALUE;
+        
+        for (int n : nums) {
+            if (Math.abs(n) < min) {
+                min = Math.abs(n);
+                ans = n;
+            }
+            else if (Math.abs(n) == min) {
+                ans = Math.max(ans, n);
+            }
+        }
+        
+        return ans;      
+    }
+}
+
