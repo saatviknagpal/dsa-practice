c17f8d490b84acfcd2dd4f9e3334e8221149f708
Saatvik Nagpal <saatvikn@usc.edu>
2024-08-27 16:16:19 -0700

    Time: 8 ms (85.2%), Space: 46.7 MB (17.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,19 +1,18 @@
-class Solution {
+public class Solution {
     public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) return new int[0][0];
+
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        LinkedList<int[]> merged = new LinkedList<>();
+
+        List<int[]> merged = new ArrayList<>();
         for (int[] interval : intervals) {
-            // if the list of merged intervals is empty or if the current
-            // interval does not overlap with the previous, simply append it.
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
                 merged.add(interval);
-            }
-            // otherwise, there is overlap, so we merge the current and previous
-            // intervals.
-            else {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
             }
         }
+
         return merged.toArray(new int[merged.size()][]);
     }
 }
