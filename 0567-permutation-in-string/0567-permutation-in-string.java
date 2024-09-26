07e5596347efb3770504a55e53fb49ab9a425b6a
Saatvik Nagpal <saatvikn@usc.edu>
2024-09-26 15:59:27 -0700

    Time: 4 ms (99.18%), Space: 42.3 MB (89.21%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,41 +1,19 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character, Integer> mp = new HashMap<>();
 
-        for (int i = 0; i < s1.length(); i++) {
-            char c = s1.charAt(i);
-            mp.put(c, mp.getOrDefault(c, 0) + 1);
-        }
-
-        int remainingChars = s1.length();
-
-        for (int i = 0; i < s2.length(); i++) {
-            char c = s2.charAt(i);
-
-            if (mp.containsKey(c)) {
-                mp.put(c, mp.get(c) - 1);
-                if (mp.get(c) >= 0) {  // Adjust this condition
-                    remainingChars--;
-                }
-            }
-
-            if (remainingChars == 0) {
-                return true;
-            }
-
-            if (i >= s1.length() - 1) {
-                char startChar = s2.charAt(i - (s1.length() - 1));
-                if (mp.containsKey(startChar)) {
-                    mp.put(startChar, mp.get(startChar) + 1);
-                    if (mp.get(startChar) > 0) {
-                        remainingChars++;
-                    }
-                }
-            }
-        }
-
+        int m = s1.length(), map[] = new int[26];        
+        for (int i = 0; i < m; i++) map[s1.charAt(i)-'a']--;
+        
+        for (int i = 0, j = 0, len = 0; j < s2.length(); j++) {
+            int k = s2.charAt(j)-'a';
+            map[k]++;
+            
+            while (map[k] > 0) {                
+                map[s2.charAt(i++)-'a']--;                    
+            }    
+                                                
+            if (j-i+1 == m) return true;            
+        }        
         return false;
     }
 }
