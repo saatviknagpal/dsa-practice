df48b98cee8adac32cc882b9cc67d71c55a4cc86
Saatvik Nagpal <saatvikn@usc.edu>
2025-06-01 17:38:35 -0700

    Time: 3 ms (84.79%), Space: 46.4 MB (10.46%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,19 +1,39 @@
 class Solution {
-
     public int candy(int[] ratings) {
-        int candies[] = new int[ratings.length];
-        Arrays.fill(candies, 1); 
+        int i = 1;
+        int sum = 1;
 
-        for (int i = 1; i < candies.length; i++) { 
-            if (ratings[i] > ratings[i - 1]) candies[i] = (candies[i - 1] + 1);
+
+        while(i < ratings.length){
+
+            if(ratings[i] == ratings[i-1]){
+                sum += 1;
+                i++;
+                continue;
+            }
+
+            int peak = 1;
+
+            while(i < ratings.length && ratings[i] > ratings[i-1]){
+                peak += 1;
+                sum += peak;
+                i++;
+            }
+
+            int down = 0;
+
+            while(i < ratings.length && ratings[i] < ratings[i-1]){
+                down +=1;
+                sum += down;
+                i++;
+            }
+            down+=1;
+            if(down > peak){
+                sum += down - peak;
+            }
         }
 
-        for (int i = candies.length - 2; i >= 0; i--) { 
-            if (ratings[i] > ratings[i + 1]) candies[i] = Math.max(candies[i], (candies[i + 1] + 1));
-        }
-
-        int sum = 0;
-        for (int candy : candies) sum += candy;
         return sum;
+
     }
 }
