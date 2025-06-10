dbd5ac7006928e5a72ec3263db1d72709bc191c3
Saatvik Nagpal <saatvikn@usc.edu>
2025-06-10 16:35:54 -0700

    Time: 1 ms (100%), Space: 42.3 MB (89.17%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
@@ -12,11 +12,8 @@ class Solution {
 
         for(int i = 0; i < freq.length; i++){
             if(freq[i] == 0) continue;
-            System.out.println(freq[i]);
             if(max < freq[i] && freq[i] % 2 != 0) max = freq[i];
-            System.out.println(max + " " + i);
             if(min > freq[i] && freq[i] % 2 == 0) min = freq[i];
-            System.out.println(min + " " + i);
         }
 
         return max - min;
