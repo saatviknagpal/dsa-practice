321faa293276f9c2a3cff4e7d1b70a5f2daca929
Saatvik Nagpal <saatvikn@usc.edu>
2025-07-14 16:40:39 -0700

    Time: 0 ms (100%), Space: 42.7 MB (45.97%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        ListNode l1 = list1, l2 = list2;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (l1 != null) ? l1 : l2;
+
+        return dummy.next;
+    }
+}
