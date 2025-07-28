f88426e8b7e7b98242a88e698d976ab560df6458
Saatvik Nagpal <saatvikn@usc.edu>
2025-07-27 19:06:12 -0700

    Time: 0 ms (100%), Space: 40.8 MB (97.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,14 +14,18 @@
  * }
  */
 class Solution {
-
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
-        boolean left = isSameTree(p.left, q.left);
-        boolean right = isSameTree(p.right, q.right);
 
-        return left && right;
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+
+        boolean lf = isSameTree(p.left, q.left);
+        boolean rf = isSameTree(p.right, q.right);
+
+        return lf && rf;
     }
 }
