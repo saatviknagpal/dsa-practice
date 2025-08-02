5d1b8b9750ccd2f40d31b6786a034a50e1d20099
Saatvik Nagpal <saatvikn@usc.edu>
2025-08-02 11:33:28 -0700

    Time: 1 ms (49.45%), Space: 42.4 MB (98.84%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,20 @@
+public class Solution {
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        dfs(nums, 0, subset, res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int i, List<Integer> subset, List<List<Integer>> res) {
+        if (i >= nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[i]);
+        dfs(nums, i + 1, subset, res);
+        subset.remove(subset.size() - 1);
+        dfs(nums, i + 1, subset, res);
+    }
+}
