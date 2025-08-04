5bfb79598e47b280aeb3ea72291664f0d5d4c029
Saatvik Nagpal <saatvikn@usc.edu>
2025-08-04 16:33:51 -0700

    Time: 80 ms (19.62%), Space: 53.5 MB (19.54%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,36 +1,35 @@
-public class Solution {
-    public static List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> result = new ArrayList<>();
-        Set<String> preWords = new HashSet<>();
-        Arrays.sort(words, new Comparator<String>() {
-            public int compare (String s1, String s2) {
-                return s1.length() - s2.length();
+class Solution {
+    public boolean isConcatenated(String word, Set<String> st, Map<String, Boolean> map) {
+        if (map.containsKey(word))
+            return map.get(word);
+        int l = word.length();
+        for (int i = 0; i < l; i++) {
+            String prefix = word.substring(0, i + 1);
+            String suffix = word.substring(i + 1, l);
+            if ((st.contains(prefix) && st.contains(suffix))
+                    || (st.contains(prefix) && isConcatenated(suffix, st, map))) {
+                map.put(word, true);
+                return true;
             }
-        });
-        
-        for (int i = 0; i < words.length; i++) {
-            if (canForm(words[i], preWords)) {
-                result.add(words[i]);
-            }
-            preWords.add(words[i]);
         }
-        
-        return result;
+        map.put(word, false);
+        return false;
     }
-	
-    private static boolean canForm(String word, Set<String> dict) {
-        if (dict.isEmpty()) return false;
-	boolean[] dp = new boolean[word.length() + 1];
-	dp[0] = true;
-	for (int i = 1; i <= word.length(); i++) {
-	    for (int j = 0; j < i; j++) {
-		if (!dp[j]) continue;
-		if (dict.contains(word.substring(j, i))) {
-		    dp[i] = true;
-		    break;
-		}
-	    }
-	}
-	return dp[word.length()];
+
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        int n = words.length;
+        Set<String> st = new HashSet<>();
+        Map<String, Boolean> map = new HashMap<>();
+        for (String s : words)
+            st.add(s);
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+
+            if (isConcatenated(word, st, map)) {
+                result.add(word);
+            }
+        }
+        return result;
     }
 }
