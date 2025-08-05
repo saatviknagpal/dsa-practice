b3932951828350594c67e2da8635fdda459c823c
Saatvik Nagpal <saatvikn@usc.edu>
2025-08-04 17:03:19 -0700

    Time: 0 ms (100%), Space: 43.2 MB (70.42%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,28 +1,32 @@
 class Solution {
-    List<String> result = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        int open = 0;
+        int close = 0;
+        StringBuilder sb = new StringBuilder();
+        helper(result, n, open, close, sb);
 
-    public void solve(StringBuilder curr, int n, int open, int close) {
-        if (curr.length() == 2 * n) {
-            result.add(curr.toString());  // Convert StringBuilder to String
+        return result;
+    }
+
+    public void helper(List<String> result, int n, int open, int close, StringBuilder sb){
+        if(open == n && close == n){
+            result.add(sb.toString());
             return;
         }
 
-        if (open < n) {
-            curr.append("(");
-            solve(curr, n, open + 1, close);
-            curr.deleteCharAt(curr.length() - 1);  // backtrack
+        if(open < n){
+            sb.append('(');
+            helper(result, n, open + 1, close, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
 
-        if (close < open) {
-            curr.append(")");
-            solve(curr, n, open, close + 1);
-            curr.deleteCharAt(curr.length() - 1);  // backtrack
+        if(close < open){
+            sb.append(')');
+            helper(result, n, open, close + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 
-    public List<String> generateParenthesis(int n) {
-        StringBuilder curr = new StringBuilder();
-        solve(curr, n, 0, 0);
-        return result;
-    }
+
 }
